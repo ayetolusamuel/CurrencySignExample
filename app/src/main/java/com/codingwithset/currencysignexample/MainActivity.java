fcb390365e4b2ab8daacb03e2478d56c854b2aa8
ayetolusamuel <ayetolusamuel@gmail.com>
2019-12-12 10:30:38 +0100

    Activate CurrencySign

--- app/src/main/java/com/codingwithset/currencysignexample/MainActivity.java
+++ app/src/main/java/com/codingwithset/currencysignexample/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.codingwithset.currencysign.CurrencySign;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CurrencySign currencySign = new CurrencySign();
+
 
     }
 
--- currencysign/src/main/java/com/codingwithset/currencysign/CurrencySign.java
+++ currencysign/src/main/java/com/codingwithset/currencysign/CurrencySign.java
@@ -5,6 +5,7 @@ import android.content.Context;
 public class CurrencySign {
     private static String inNaira(Context context, String price) {
         String actualPrice = Utils.formatPrice(context, price);
-        return context.getString(R.string.product_price, actualPrice);
+        String format = String.format("₦%1$s",actualPrice);
+        return format;
     }
 }
