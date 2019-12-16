55c5e1d4cc924c27ae254c7f2f36b1e8a592b088
ayetolusamuel <ayetolusamuel@gmail.com>
2019-12-16 10:59:25 +0100

    change the price format

--- currencysign/src/main/java/com/codingwithset/currencysign/CurrencyFormat.java
+++ currencysign/src/main/java/com/codingwithset/currencysign/CurrencyFormat.java
@@ -4,19 +4,20 @@ import android.content.Context;
 
 import java.text.DecimalFormat;
 
- class CurrencyFormat {
+class CurrencyFormat {
 
-     static String formatPriceInThousand(Context context, String price) {
+    static String formatPriceInThousand(Context context, String price) {
+        price = price.replace(context.getString(R.string.comma), context.getString(R.string.empty));
         double format = Double.parseDouble(price);
         DecimalFormat formatter = new DecimalFormat(context.getString(R.string.pattern));
         return formatter.format(format);
-
     }
-      static String truncateDecimalPrice(Context context,String actualPrice) {
-         if (actualPrice.contains(context.getString(R.string.zeo_after_decimal))){
-             actualPrice = actualPrice.replace(context.getString(R.string.zeo_after_decimal),context.getString(R.string.empty));
-         }
-         return actualPrice;
-     }
+
+    static String truncateDecimalPrice(Context context, String actualPrice) {
+        if (actualPrice.contains(context.getString(R.string.zero_after_decimal))) {
+            actualPrice = actualPrice.replace(context.getString(R.string.zero_after_decimal), context.getString(R.string.empty));
+        }
+        return actualPrice;
+    }
 
 }
